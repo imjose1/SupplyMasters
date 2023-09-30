3d377765fbd9c5e6cede39760d7dc91c9851e86f
imjose1 <jose_ghj@hotmail.com>
2023-09-29 23:06:06 -0500

    Initial commit

---
+++ SupplyMasters/Controllers/InventoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SupplyMasters.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
